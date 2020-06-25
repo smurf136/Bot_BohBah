603fc475d874d29c18f98085f88d1b6defeea7c0
Smurf136 <apiwat.it24@mail.kmutt.ac.th>
2020-06-26 01:47:06 +0700

    [Add] firebase connection

---
+++ src/firebase.js
@@ -0,0 +1,16 @@
+let firebase = require('firebase');
+
+let firebaseConfig = {
+  apiKey: "AIzaSyDlHErRWFW4c44evIpP97ZNHKUM2S5MeAA",
+  authDomain: "bot-bohbah.firebaseapp.com",
+  databaseURL: "https://bot-bohbah.firebaseio.com",
+  projectId: "bot-bohbah",
+  storageBucket: "bot-bohbah.appspot.com",
+  messagingSenderId: "474991597697",
+  appId: "1:474991597697:web:497e517c269b8298079580",
+  measurementId: "G-BEV88WX08L"
+};
+
+firebase.initializeApp(firebaseConfig)
+
+module.exports = { firebase }
