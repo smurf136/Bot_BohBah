69c6e3269492348c91f77479020cfffc3a0f121b
first-unknow <firstpan555@gmail.com>
2020-06-26 01:44:31 +0700

    [ADD]ReadCSV to JSON

---
+++ src/readcsv.js
@@ -0,0 +1,16 @@
+const csv = require('csv-parser');
+const fs = require('fs');
+
+function readcsv(csvFile) {
+  fs.createReadStream(csvFile)
+    .pipe(csv())
+    .on('data', (row) => {
+      console.log(row);
+    })
+    .on('end', () => {
+      console.log('CSV file successfully processed');
+    });
+}
+
+
+module.exports = { readcsv }
