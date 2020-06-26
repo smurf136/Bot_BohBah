3b2f2431d74f17398dcb397aadeecfe41fac5a45
TipsukonCheer <tipsukon.cheer@mail.kmutt.ac.th>
2020-06-26 23:17:23 +0700

    [UPDATE] add flow2 mock data and function

--- src/database.js
+++ src/database.js
@@ -13,6 +13,39 @@ const words = {
   'กาแฟ': ['เอาไว้กินแก้ง่วง'],
 }
 
+const flow2Words = {
+  'คนสุดท้ายที่อยู่ในเกม' : ['คนสุดท้าย','เกม'],
+  'หมายถึงชอบเรา' : ['ชอบ'],
+  'ไอจามไม่มีไอจีดีกว่า' : ['ไอ','จาม'],
+  'ชงมุขอะน่ะ' : ['ชง']
+}
+
+function getFlowTwoAllKeys(){
+  let data = {}
+  let key = {} 
+  let all = []
+  let allKeys = Object.keys(flow2Words)
+  for(let e in allKeys){
+    for(let i in flow2Words[allKeys[e]]){
+      all.push(flow2Words[allKeys[e]][i])
+    }
+    key[flow2Words[allKeys[e]][0]] = flow2Words[allKeys[e]][1] == null ? 0 : flow2Words[allKeys[e]][1];
+  }
+  data = {
+    allKeys : all,
+    keys : key
+  }
+  return data;
+}
+
+function getFlowTwoAnswer(value){
+  return Object.keys(flow2Words).find((key) => key.includes(value));
+}
+
+function getFlowTwoDescription(key){
+  return flow2Words[key];
+}
+
 function getAllWords() {
   return Object.keys(words);
 }
@@ -21,4 +54,10 @@ function getWordDescriptions(word) {
   return words[word];
 }
 
-module.exports = { getAllWords, getWordDescriptions };
+module.exports = { 
+  getAllWords, 
+  getWordDescriptions,
+  getFlowTwoAllKeys,
+  getFlowTwoAnswer,
+  getFlowTwoDescription
+};
