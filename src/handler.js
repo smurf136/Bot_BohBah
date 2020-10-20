de03bac6c87f3bc26deea45a6e16c29d87da2e72
pipe34150 <pipe34150@hotmail.com>
2020-10-20 19:01:13 +0700

    [UPDATE] create handler to handle new messgae with add options

--- src/handler.js
+++ src/handler.js
@@ -1,5 +1,6 @@
 const { Message } = require("discord.js");
 const { createJoke } = require("./create-joke");
+const { addJoke } = require("./add-joke");
 
 let currentJokes = {};
 
@@ -8,16 +9,26 @@ let currentJokes = {};
  * @param {Message} msg
  */
 async function handleMessage(msg) {
-  if (!currentJokes[msg.author.id]) {
-    currentJokes[msg.author.id] = await createJoke(msg.content);
-  }
-  if (currentJokes[msg.author.id]) {
-    const joke = currentJokes[msg.author.id];
-    msg.reply(joke.next(msg.content));
-    if (joke.isEnded()) {
-      delete currentJokes[msg.author.id];
+  if(isAddJoke(msg)){
+    let replyMessage = addJoke(msg.content);
+    msg.reply(replyMessage);
+  } else{
+    if (!currentJokes[msg.author.id]) {
+      currentJokes[msg.author.id] = await createJoke(msg.content);
     }
+    if (currentJokes[msg.author.id]) {
+      const joke = currentJokes[msg.author.id];
+      msg.reply(joke.next(msg.content));
+      if (joke.isEnded()) {
+        delete currentJokes[msg.author.id];
+      }
+    } 
   }
+
+}
+
+function isAddJoke(msg){
+  return msg.content.includes('-');
 }
 
 module.exports = { handleMessage };
