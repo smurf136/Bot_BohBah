c5a99da26cf6def6c6a1fbc07ea0eed62a349058
TipsukonCheer <tipsukon.cheer@mail.kmutt.ac.th>
2020-10-22 18:39:25 +0700

    change add joke condition and normal joke method

--- src/add-joke.js
+++ src/add-joke.js
@@ -7,41 +7,52 @@ const { help } = require('./help');
  * @param {string} msg
  */
 
-const addJokeFormat = /^-a normalJoke [a-zA-Z]+ [a-zA-Z]+ [a-zA-Z]+/;
 
 function addJoke(msg) {
-  if(addJokeFormat.test(msg)){
-    console.log(msg)
+  if (msg.includes("normal")) {
     return addNormal(msg)
+  } else if (msg.includes("dialog")) {
+    return addDialogJoke(msg)
+  } else {
+    return "key";
   }
-  return addDialogJoke(msg);
 }
 
-function addDialogJoke(msg){ 
-  if(msg.includes('-adj')){
-    const newMessage = msg.replace('-adj ','');
-    let dialogJoke = [];
-    let string = newMessage.split(" ");
-    for(let i = 0 ; i < string.length ; i++){
-      dialogJoke.push(string[i]);
-    } 
-    if(dialogJoke.length < 2 || string.includes('help')){
-      return help(msg);
-    }
-    let key = dialogJoke.shift();
-    database.addFlowTwo(key,...dialogJoke);
-    return 'Add dialog joke successful.';
+function addDialogJoke(msg) {
+  const newMessage = msg.replace('-a dialogJoke ', '');
+  let dialogJoke = [];
+  let string = newMessage.split(" ");
+  for (let i = 0; i < string.length; i++) {
+    dialogJoke.push(string[i]);
   }
-  return help(msg);
+  if (dialogJoke.length < 2 || string.includes('help')) {
+    return help(msg);
+  }
+  let key = dialogJoke.shift();
+  let a = database.addFlowTwo(key, ...dialogJoke);
+  console.log(database.getFlowTwoAllKeys())
+  // console.log(dialogJoke);
+  return 'Succeed';
 }
 
-function addNormal(msg){
+function addNormal(msg) {
   let splitString = msg.split(" ");
-    let answer = splitString[2];
-    let description = splitString[3];
-    let word = splitString[4];
-    addNormalJoke(answer,description,word);
-    return "Add normal joke successful.";
+  let answer = splitString[2];
+  let description = splitString[3];
+  let word = splitString[4];
+  console.log("eieiei")
+  if(!(answer||description||word)){
+    return "plese enter answer, description, word";
+  }
+  console.log("elelele")
+  addNormalJoke(answer, description, word);
+  return "add Success";
 }
 
+function help(msg) {
+  if (msg.includes('-adj')) {
+    return 'Use: -adj [answer] [keyword] [..keyword] to create new joke.';
+  }
+  return 'You just type something wrong, try again.'
+}
 module.exports = { addJoke };
